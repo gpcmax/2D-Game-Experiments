65410aee1aae0ae0db5d1acc472de48d9f3e6e52
Gage <gage.copelin@gmail.com>
2019-01-27 18:15:25 -0500

    removed some assets and messed around with a grid system

---
+++ 2D Experiments/Assets/GridManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour {
+
+    public float x_start, y_start;
+    public int columLength, rowLength;
+    public float x_space, y_space;
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < columLength * rowLength; i++)
+        {
+            Instantiate(prefab, new Vector3(x_start +( x_space * (i % columLength)),y_start+ (-y_space * (i / columLength))), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
